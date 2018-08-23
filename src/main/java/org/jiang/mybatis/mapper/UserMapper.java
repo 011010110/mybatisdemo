a1ecf34b4edcbe443ad5af608d873884940d57e5
lilinhua <582966876@qq.com>
2018-08-24 00:22:25 +0800

    Mybatis传参数测试

--- src/main/java/org/jiang/mybatis/mapper/UserMapper.java
+++ src/main/java/org/jiang/mybatis/mapper/UserMapper.java
@@ -1,11 +1,29 @@
 package org.jiang.mybatis.mapper;
 
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Param;
 import org.jiang.mybatis.entity.User;
 
 public interface UserMapper {
 
 	
 	User getUserById(String id);
+	
+	User getUserByIdName(String id,String name);
+	
+	User getUserByIdMapParamParamAnno(@Param("id")String id,@Param("name")String name);
+	
+	User getUserByIdMapParam(@Param("id")String id,@Param("mapParam")Map<String,Object> mapParam);
+	
+	User getUserByMapParam(Map<String,Object> mapParam);
+	
+	User getUserByObjectUserMap(@Param("user")User user,@Param("mapParam")Map<String,Object> param);
+	
+	User getUserByListParam(List<Map<String,Object>> listParam);
+	
+	User getUserByIdReturnResultMap(String id);
 	
 	long insertUser(User user);
 	
--- src/test/java/org/jiang/mybatis/test/XmlMapperTest.java
+++ src/test/java/org/jiang/mybatis/test/XmlMapperTest.java
@@ -2,20 +2,29 @@ package org.jiang.mybatis.test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.jiang.mybatis.entity.User;
+import org.jiang.mybatis.mapper.UserMapper;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class XmlMapperTest {
 
 	SqlSessionFactory sqlSessionFactory;
 	SqlSession sqlSession;
+	Logger logger = LoggerFactory.getLogger("xmlMapperTest===========================");
 	private String uuid = "51cfad92-20ef-4c49-8503-c4ec6f8ef819-test";
 	@Before
 	public void beforeInit() {
@@ -32,9 +41,10 @@ public class XmlMapperTest {
 	
 	@After
 	public void after() {
-		
 		sqlSession.close();
 	}
+	
+	
 	@Test
 	public void insertUserTest() {
 		User user = new User();
@@ -48,13 +58,13 @@ public class XmlMapperTest {
 	
 	@Test
 	public void xmlSelectTest() {
-		User user = sqlSession.selectOne("org.jiang.mybatis.mapper.UserMapper.getUserById", uuid);
+		Map<String,Object> params = new HashMap<>();
+		params.put("username", "mybatisTest2");
+		User user = sqlSession.selectOne("org.jiang.mybatis.mapper.UserMapper.getUserById", params);
 		if(user!=null) {
 			System.out.println(user.toString());
 		}
 	}
-	
-	
 	
 	@Test
 	public void updateUserTest() {
@@ -74,5 +84,107 @@ public class XmlMapperTest {
 		System.out.println("result="+result);
 		sqlSession.commit();
 	}
+	
+	@Test
+	public void selectUserByIdTest() {
+		UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+		User user = userMapper.getUserById("51cfad92-20ef-4c49-8503-c4ec6f8ef819-test");
+		Assert.assertNotNull(user);
+		logger.debug("userinfo:"+user.toString());
+		System.out.println(user.toString());
+	}
+	/**
+	 * 测试多个参数
+	 */
+	@Test
+	public void getUserByIdNameTest() {
+		UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+		User user = userMapper.getUserByIdName("51cfad92-20ef-4c49-8503-c4ec6f8ef819-test","mybatisTest23");
+		Assert.assertNotNull(user);
+		logger.debug("userinfo:"+user.toString());
+		System.out.println(user.toString());
+	}
+	/**
+	 * 测试map参数
+	 */
+	@Test
+	public void getUserByMapParamTest() {
+		UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+		Map<String,Object> param = new HashMap<>();
+		param.put("id", "51cfad92-20ef-4c49-8503-c4ec6f8ef819-test");
+		param.put("name", "mybatisTest23");
+		User user = userMapper.getUserByMapParam(param);
+		Assert.assertNotNull(user);
+		logger.debug("userinfo:"+user.toString());
+		System.out.println(user.toString());
+	}
+	/**
+	 * 测试list参数
+	 */
+	@Test
+	public void getUserByListParamTest() {
+		UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+		Map<String,Object> param = new HashMap<>();
+		param.put("id", "51cfad92-20ef-4c49-8503-c4ec6f8ef819-test");
+		param.put("name", "mybatisTest23");
+		List<Map<String,Object>> paramList = new ArrayList<>();
+		paramList.add(param);
+		User user = userMapper.getUserByListParam(paramList);
+		Assert.assertNotNull(user);
+		logger.debug("userinfo:"+user.toString());
+		System.out.println(user.toString());
+	}
+	/**
+	 * 测试多个参数,添加@Param注解
+	 */
+	@Test
+	public void getUserByIdMapParamParamAnnoTest() {
+		UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+		User user = userMapper.getUserByIdMapParamParamAnno("51cfad92-20ef-4c49-8503-c4ec6f8ef819-test","mybatisTest23");
+		Assert.assertNotNull(user);
+		logger.debug("userinfo:"+user.toString());
+		System.out.println(user.toString());
+	}
+	/**
+	 * 测试多个参数,添加且包含map类型参数
+	 */
+	@Test
+	public void getUserByIdMapParamTest() {
+		UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+		Map<String,Object> param = new HashMap<>();
+		param.put("name", "mybatisTest23");
+		User user = userMapper.getUserByIdMapParam("51cfad92-20ef-4c49-8503-c4ec6f8ef819-test",param);
+		Assert.assertNotNull(user);
+		logger.debug("userinfo:"+user.toString());
+		System.out.println(user.toString());
+	}
+	
+	/**
+	 * 测试多个参数,参数分别为POJO与Map
+	 */
+	@Test
+	public void getUserByObjectUserMapTest() {
+		UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+		User paramUser = new User();
+		paramUser.setId("51cfad92-20ef-4c49-8503-c4ec6f8ef819-test");
+		Map<String,Object> param = new HashMap<>();
+		param.put("name", "mybatisTest23");
+		User user = userMapper.getUserByObjectUserMap(paramUser,param);
+		Assert.assertNotNull(user);
+		logger.debug("userinfo:"+user.toString());
+		System.out.println(user.toString());
+	}
+	/**
+	 * 测试ResultMap
+	 */
+	@Test
+	public void getUserByIdReturnResultMapTest() {
+		UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+		User user = userMapper.getUserByIdReturnResultMap("51cfad92-20ef-4c49-8503-c4ec6f8ef819-test");
+		Assert.assertNotNull(user);
+		logger.debug("userinfo:"+user.toString());
+		System.out.println(user.toString());
+	}
+	
 	
 }
